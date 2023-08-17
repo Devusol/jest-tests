9fa627b574f1ece722fb3f24870d72463a39c34f
devusol <sean@devusol.com>
2023-08-17 08:08:55 -0400

    test import of hello

--- start.test.js
+++ start.test.js
@@ -1,5 +1,13 @@
+import {hello} from "./hello"
+
 describe('webhooks', () => {
     it('returns true', () => {
         expect(true).toEqual(true)
+    })
+})
+
+describe('hello', ()=>{
+    it('returns "Hello World', ()=>{
+        expect(hello()).toBe("Hello World")
     })
 })
